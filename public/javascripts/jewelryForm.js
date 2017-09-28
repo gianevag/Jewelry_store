aba6ae1091521f2e54f72e353897c8abf59012c8
John <gianevag@hotmail.com>
2017-09-28 22:58:09 +0300

    Make the the post of jewelry Table

--- public/javascripts/jewelryForm.js
+++ public/javascripts/jewelryForm.js
@@ -97,13 +97,35 @@ $(document).ready(function() {
             formData.append('price_14K', $('#pricesK14').text()),
             formData.append('price_18K', $('#pricesK18').text())
 
-            formData.append('gold_metal_price', JSON.stringify({
-                price_9K : $('#gold_priceK9').text(),
-                price_14K : $('#gold_priceK14').text(),
-                price_18K : $('#gold_priceK18').text(),                
-            }) )
+            formData.append('gold_priceK9', $('#gold_priceK9').text());
+            formData.append('gold_priceK14', $('#gold_priceK14').text());
+            formData.append('gold_priceK18', $('#gold_priceK18').text());
+
+            formData.append('retail_price_eurK9', $('#retail_price_eurK9').text());
+            formData.append('retail_price_eurK14', $('#retail_price_eurK14').text());
+            formData.append('retail_price_eurK18', $('#retail_price_eurK18').text());
+
+            formData.append('retail_price_dolK9', $('#retail_price_dolK9').text());
+            formData.append('retail_price_dolK14', $('#retail_price_dolK14').text());
+            formData.append('retail_price_dolK18', $('#retail_price_dolK18').text());
+
+            formData.append('commission_etsyK9', $('#commission_etsyK9').text());
+            formData.append('commission_etsyK14', $('#commission_etsyK14').text());
+            formData.append('commission_etsyK18', $('#commission_etsyK18').text());
+
+            formData.append('costK9', $('#costK9').text());
+            formData.append('costK14', $('#costK14').text());
+            formData.append('costK18', $('#costK18').text());
             
-            console.log(formData.get('gold_metal_price'));                 
+            formData.append('taxisK9', $('#taxisK9').text());
+            formData.append('taxisK14', $('#taxisK14').text());
+            formData.append('taxisK18', $('#taxisK18').text());
+            
+            formData.append('incomesK9', $('#incomesK9').text());
+            formData.append('incomesK14', $('#incomesK14').text());
+            formData.append('incomesK18', $('#incomesK18').text());
+
+                
             // process the form
             $.ajax({
                 type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
@@ -119,7 +141,7 @@ $(document).ready(function() {
                 // using the done promise callback
                 .done(function(data) {
                     console.log(data)
-                    //window.location = data.redirect;
+                    window.location = data.redirect;
     
                     // here we will handle errors and validation messages
                 });
